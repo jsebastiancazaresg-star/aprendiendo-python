baae90567ce990ed97e3a0861d24f2aee0bd4837
sebastian <jsebastiancazaresg@gmail.com>
2026-05-06 18:56:16 -0500

    aprendi a desempaquetar una tupla

---
+++ diccionaro.py
@@ -0,0 +1,16 @@
+"""
+person_1={
+    "name":"Sebastian",
+    "age":13,
+    "amigos":["Santiago","Pedro", "Eduardo", "Alvaro"],
+    "mascotas": ("Coco","Benito","Edgar")
+}
+#print("mascotas" in person_1)
+"""
+person_1={
+    "name":"Sebastian",
+    "age":13,
+    "amigos":["Santiago","Pedro", "Eduardo", "Alvaro"],
+    "mascotas": ("Coco","Benito","Edgar")
+}
+print(person_1["mascotas"])
